95c02aaa1d81896135aac251a1a208f01fcae59e
IWIH <iwih90@gmail.com>
2018-03-02 14:49:28 +0300

    add .net core project

---
+++ netcore_self_driven_ride/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace netcore_self_driven_ride
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
